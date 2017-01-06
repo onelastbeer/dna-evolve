08a59f834c5718abc68a213e766b7a04fa20d566
Louis Merlin <louis.merlin@epfl.ch>
2017-01-06 15:27:22 +0100

    Create basic go project

---
+++ dna/dna.go
@@ -0,0 +1,12 @@
+package dna
+
+import "fmt"
+
+type DNA struct {
+	MutationRate float64
+	genes        []float64
+}
+
+func CreateDNA() {
+	fmt.Println("DeeNAy")
+}
---
+++ dna/dna_test.go
@@ -0,0 +1,3 @@
+package dna
+
+import "testing"
