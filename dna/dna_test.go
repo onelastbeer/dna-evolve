dacf9a80dddc5c42c409c3b459e8b32893b5ded4
Louis Merlin <louis.merlin@epfl.ch>
2017-01-09 13:11:35 +0100

    Create skeleton of DNA testing

--- dna/dna_test.go
+++ dna/dna_test.go
@@ -1,3 +1,15 @@
 package dna
 
 import "testing"
+
+func TestLength(t *testing.T) {}
+
+func TestGene(t *testing.T) {}
+
+func TestString(t *testing.T) {}
+
+func TestRandDNA(t *testing.T) {}
+
+func TestCrossover(t *testing.T) {}
+
+func TestMutate(t *testing.T) {}
